bfb9626ca53256e5a3d489786f9e740223ffed49
KubaJerz <koobman.kj@gmail.com>
2025-03-28 01:03:40 -0400

    yolo from scratch

---
+++ week02/model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class RegHead(nn.Module):
+    def __init__(self,in_channels, S, B, dropout=0.0):
+        super().__init__()
+        self.conv0 = nn.Conv2d(in_channels, 1024, 3, padding=1)
+        self.conv1 = nn.Conv2d(1024, 1024, 3, padding=1)
+        self.conv2 = nn.Conv2d(1024, 1024, 3, padding=1)
+        self.conv3 = nn.Conv2d(1024, 1024, 3, padding=1)
+
+        self.l0 = nn.Linear((1024 * S * S), 4096)
+        self.drop = nn.Dropout(dropout)
+        self.act = nn.LeakyReLU(0.1)
+        self.l1 = nn.Linear(4096, (S * S * (B * 5)))
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x = x.flatten(start_dim=1)
+        x = self.l0(x)
+        x = self.drop(x)
+        x = self.act(x)
+        x = self.l1(x)
+        return x
+
+
+
+
+class FaceDetector(nn.Module):
+    def __init__(self, S=7, B=2):
+        super(FaceDetector, self).__init__()
+
+        backbone = models.resnet50(pretrained=True)
+        self.backbone = torch.nn.Sequential(*(list(backbone.children())[:-2]))
+        
+        self.regression_head = RegHead(2048, S, B)
+    
+    def forward(self, x):
+        features = self.backbone(x)
+        out = self.regression_head(features)
+        return out
---
+++ week02/yoloy_dataset.py
@@ -0,0 +1,43 @@
+
+import torch
+from torch.utils.data import Dataset
+
+class YoloDataset(Dataset):
+    def __init__(self, path, S=7, B=2, C = 0):
+        super().__init__()
+        self.X , self.y = torch.load(path)
+        self.S = S
+        self.B = B
+        self.C = C
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, index):
+        image = self.X
+        boxes = self.y
+        labels = torch.zeros((self.S, self.S, self.C + 5))
+        
+        # single box 
+        if boxes.dim() == 1 and len(boxes) == 4:
+            x, y, w, h = boxes.tolist()
+            i, j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
+            w_cell, h_cell = w * self.S, h * self.S
+            
+            if labels[i, j, 0] == 0:
+                labels[i, j, 0] = 1
+                labels[i, j, 1:5] = torch.tensor([x_cell, y_cell, w_cell, h_cell])
+        # multiple boxes
+        elif boxes.dim() > 1:
+            for unscaled_box in boxes:
+                x, y, w, h = unscaled_box.tolist()
+                i, j = int(self.S * y), int(self.S * x)
+                x_cell, y_cell = self.S * x - j, self.S * y - i
+                w_cell, h_cell = w * self.S, h * self.S
+                
+                if labels[i, j, 0] == 0:
+                    labels[i, j, 0] = 1
+                    labels[i, j, 1:5] = torch.tensor([x_cell, y_cell, w_cell, h_cell])
+        
+        return image, labels
